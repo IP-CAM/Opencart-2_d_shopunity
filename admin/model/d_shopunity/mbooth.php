a800424a7a85365be15f659a8b63d4a27bef0ddf
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2016-09-04 17:38:58 +0300

    no message

--- admin/model/d_shopunity/mbooth.php
+++ admin/model/d_shopunity/mbooth.php
@@ -246,6 +246,7 @@ class ModelDShopunityMbooth extends Model {
                         if(strpos($file, 'vqmod') !== false){
                             @unlink($this->dir_root.'vqmod/mods.cache');
                             $content = file_get_contents(HTTP_CATALOG);
+                            $content = file_get_contents(HTTP_CATALOG);
                         }
                     } else {
                         $result['error'][] = $file;
