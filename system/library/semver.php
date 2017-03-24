a6a32b2e804e9c6dc89e558a5a1afe4e3cd58c94
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2017-03-24 10:11:45 -0400

    v3.0.47 fix ^

--- system/library/semver.php
+++ system/library/semver.php
@@ -9,12 +9,21 @@ class Semver {
 		return Semver\Comparator::lessThan($v1, $v2);
 	}
 
+	public function sort(array $v1){
+		return Semver\Semver::sort($v1);
+	}
+
+	public function rsort(array $v1){
+		return Semver\Semver::rsort($v1);
+	}
 
 	public function satisfies($v1, $v2){
+		$v2 = str_replace('^ ', '^', $v2);
 		return Semver\Semver::satisfies($v1, $v2);
 	}
 
 	public function satisfiedBy(array $v1, $v2){
+		$v2 = str_replace('^ ', '^', $v2);
 		return Semver\Semver::satisfiedBy($v1, $v2);
 	}
 
