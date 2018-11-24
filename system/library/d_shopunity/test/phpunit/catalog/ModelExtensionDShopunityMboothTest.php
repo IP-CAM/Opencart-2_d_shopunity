b93721f42931b2e9e3f560480bd9e8bace220907
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-11-25 01:32:30 +0200

    added tests

---
+++ system/library/d_shopunity/test/phpunit/catalog/ModelExtensionDShopunityMboothTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ModelExtensionDShopunityMboothTest extends OpenCartTest
+{
+    public function test_if_extension_returns_valid_json()
+    {   
+        $this->load->model('extension/d_shopunity/mbooth');
+        $output = $this->model_extension_d_shopunity_mbooth->_extension(json_decode(file_get_contents(DIR_SYSTEM . 'library/d_shopunity/extension/d_shopunity.json'), true));
+        $this->assertTrue(
+            isset($output['codename']) 
+            && isset($output['version'])
+            && isset($output['name'])
+            && isset($output['description'])
+            && isset($output['opencart_version'])
+            && isset($output['type'])
+            && isset($output['install'])
+            && isset($output['uninstall'])
+            && isset($output['files'])
+            && isset($output['changelog'])
+        );
+        $this->assertEquals('extension/d_shopunity', $output['index'] );
+        
+    }
+}
